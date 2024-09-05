de1032b37ca0b7ba48fe0d33d588a39a7df58055
min <minull1810@gmail.com>
2024-09-06 05:25:54 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ import { Loading } from "./core/components/index.ts";
 
 i18next.init({
     interpolation: { escapeValue: false },
-    lng: "vi",
+    lng: "en",
     resources: {
         en: {
             global: global_en,
