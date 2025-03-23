5bc0fa69268f2fa3006ea97909ae3855c0be31b5
min <minull1810@gmail.com>
2025-03-23 08:03:37 +0700

    updates

--- src/core/components/customs/Account/index.tsx
+++ src/core/components/customs/Account/index.tsx
@@ -5,8 +5,8 @@ import Images from "~/assets/imgs";
 const Account: React.FC = () => {
     return (
         <div className="account__container">
-            <img src={Images.account} alt="Vo Ngoc Min Kien" />
-            <p>Võ Ngọc Min Kiên</p>
+            <img src={Images.account} alt="Vo Ngoc Min" />
+            <p>Võ Ngọc Min</p>
         </div>
     );
 };
